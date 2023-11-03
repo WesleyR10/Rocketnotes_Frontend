f58885da84595d12526ef7a6822eb8f7e484b9ea
WesleyR10 <wesleyribas2015@gmail.com>
2023-11-03 16:22:59 -0300

    Redirection - context

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,13 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { FiUser, FiMail, FiLock } from 'react-icons/fi'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
-
 import { Container, Form, Background } from "./styles";
 
+import { api } from "../../services/api";
 
 export function SignUp() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    //console.log(name, email, password)
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos!")
+    }
+
+    api.post("/users", { name, email, password }).then(() => {
+      alert("Usuário cadastrado com sucesso!");
+      navigate("/")
+    }).catch(error => {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível cadastrar.")
+      }
+    });
+  }
+
   return (
     <Container>
       <Background />
@@ -22,21 +47,24 @@ export function SignUp() {
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
         <Link to="/">Voltar para o login</Link>
       </Form>
